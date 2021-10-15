812fe219e1450223e149633f01e8b2b39acae22f
efratWaiss <efrat.waiss@outlook.co.il>
2021-10-15 15:18:14 +0300

    initial code + welcome functions

---
+++ dotNet5782_9071_7106/Targil0/Program9071.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome9071();
+            Wellcome7106();
+            Console.ReadKey();
+        }
+        static partial void Wellcome7106();
+        private static void welcome9071()
+        {
+            Console.Write("Enter your name: ");
+            string a = Console.ReadLine();
+            Console.Write("{0}, welcome to my first console application", a);
+        }
+    }
+}
