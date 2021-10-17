253aaef1628dc58322903c12e5b7400451649abf
AyeletWeits <mnweits@gmail.com>
2021-10-18 00:56:44 +0300

    dear efra, the funktion workings

---
+++ dotNet5782_9071_7106/Targil0/program7106.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+  partial class program
+    {
+        static void welcome7106()
+        {
+            Console.Write("what do you wont study?");
+            string a = Console.ReadLine();
+            Console.Write("{0}, welcome to my school", a);
+        }
+    }
+}
