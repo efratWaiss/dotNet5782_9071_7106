187782d28396e31bd7c6c67914205a6dba196ac6
efratWaiss <efrat.waiss@outlook.co.il>
2021-10-28 19:55:44 +0300

    add enum class

--- dotNet5782_9071_7106/DAL/DataSource.cs
+++ dotNet5782_9071_7106/DAL/DataSource.cs
@@ -1,13 +1,27 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DAL
+namespace IDAL
 {
-    class DataSource
+    namespace DO
     {
+        internal class DataSource
+        {
+            internal List<Drone>  drones ;
+            internal static Station [] stations =new Station[5];
+            internal static Customer [] customers =new Customer[100];
+            internal static Parcel [] parcels =new Parcel[1000];
+            internal class Config 
+            {
+                
+
+            }
+
+        }
 
     }
 }
