d624ab1588072f5d8b4bd483f9278d5a4fe9f5db
AyeletWeits <mnweits@gmail.com>
2021-12-12 17:46:30 +0200

    zzz

---
+++ dotNet5782_9071_7106/BL/Exception.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace IBL.BO
+{
+    class Exception
+    {
+        [Serializable]
+        class idException : System.Exception
+        {
+            public idException() : base() { }
+            public idException(string message) : base(message) { }
+            public idException(string message, System.Exception inner)
+                        : base(message, inner) { }
+            protected idException(SerializationInfo info, StreamingContext context)
+                        : base(info, context) { }
+
+            override public string ToString()
+            { return Message; }
+        }
+        class notExistException : System.Exception
+        {
+            public notExistException() : base() { }
+            public notExistException(string message) : base(message) { }
+            public notExistException(string message, System.Exception inner)
+                        : base(message, inner) { }
+            protected notExistException(SerializationInfo info, StreamingContext context)
+                        : base(info, context) { }
+
+            override public string ToString()
+            { return Message; }
+        }
+    }
+}
---
+++ dotNet5782_9071_7106/BL/IBL.cs
@@ -0,0 +1,31 @@
+﻿using IBL.BO;
+using System;
+using System.Collections.Generic;
+
+namespace IBL
+{
+    interface IBL
+    {
+        public void addStation(Station s);
+        public void addDrone(Drone d);
+        public void addCustomer(Customer c);
+
+        public int addParcel(Parcel p);
+        public Station printStation(int id);
+        public Drone printDrone(int id);
+        public Customer printCustomer(int id);
+        public Parcel printParcel(int id);
+        public IEnumerable<Station> viewStation();
+        public IEnumerable<Drone> viewDrone();
+        public IEnumerable<Customer> viewCustomer();
+        public IEnumerable<Parcel> viewParcel();
+        public IEnumerable<Parcel> ParcelNoDrone();
+        public IEnumerable<Station> StationNoCharge();
+        public void updateParcelToDrone(int idDrone, int idParcel);
+        public void pickedUpD(int idDrone, DateTime d);
+        public void targetId(int idCustomer, int idParcel);
+        public void sendDroneToStation(int idDrone, int idStation);
+        public void freeDrone(int idDrone);
+        public double[] powerConsumpitionByDrone();
+    }
+}
