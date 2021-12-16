1b9d21e596a5ba6b907368ba395f215e70777a01
efratWaiss <efrat.waiss@outlook.co.il>
2021-12-16 23:39:43 +0200

    thisss

--- dotNet5782_9071_7106/BL/Customer.cs
+++ dotNet5782_9071_7106/BL/Customer.cs
@@ -8,20 +8,25 @@ namespace IBL.BO
         public int Id { get; set; }
         public string Name { get; set; }
         public string Phone { get; set; }
-        public double Longitude { get; set; }
-        public double Latitude { get; set; }
+        //public double Longitude { get; set; }
+        //public double Latitude { get; set; }
+        public Location Location { get; set; }
+        public List<Parcel> parcelFromCustomer { get; set; }
+        public List<Parcel> parcelToCustomer { get; set; }
         public override string ToString()
         {
-            return "id:" + Id + " Name:" + Name + " Phone:" + Phone + " Longitude:" + Longitude + " Latitude:" + Latitude;
+            return "id:" + Id + " Name:" + Name + " Phone:" + Phone + " Location" + Location + " parcelFromCustomer:" + parcelFromCustomer+ " parcelToCustomer:"+ parcelToCustomer;
         }
-        public Customer(int Id, string Name, string Phone, double Longitude, double Latitude)
+        public Customer(int Id, string Name, string Phone, Location Location, List<Parcel> parcelFromCustomer, List<Parcel> parcelToCustomer)
         {
             this.Id = Id;
             this.Name = Name;
             this.Phone = Phone;
-            this.Longitude = Longitude;
-            this.Latitude = Latitude;
+            this.Location = Location;
+            this.parcelFromCustomer = parcelFromCustomer;
+            this.parcelToCustomer = parcelToCustomer;
         }
 
     }
+
 }
