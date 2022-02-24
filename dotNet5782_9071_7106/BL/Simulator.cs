2d4a78270ff2834c2feb1fcb0348d80a893b86a3
AyeletWeits <mnweits@gmail.com>
2022-02-24 17:40:33 +0200

    ccc

--- dotNet5782_9071_7106/BL/Simulator.cs
+++ dotNet5782_9071_7106/BL/Simulator.cs
@@ -1,70 +1,71 @@
-﻿//using System;
-//using BO;
-//using System.Threading;
-//using BlApi;
+﻿using System;
+using BO;
+using System.Threading;
+using BlApi;
 
-//namespace BL
-//{
+namespace BL
+{
 
-//    class Simulator
-//    {
-//        //static System.Windows.Timer myTimer = new System.Windows.Timer();
-//        const int DELAY = 5000;// השהיה לחצי שניה
-//        public double speed;//מהירות
-//        private int idDrone;
-//        private Action a;
-//        private Func<bool> stop;
-
-
-
-//        public Simulator(IBL bl, int idDrone, Action a, Func<bool> stop)
-//        {
-//            //double battery;
-//            while (!stop())
-//            {
-//                lock (bl)
-//                {
-//                    Drone drone = bl.GetDrone(idDrone);
-//                    switch (drone.Status)
-//                    {
-//                        case DroneStatuses.Vacant:
-//                            {
-//                                try
-//                                {
-//                                    bl.UpdateParcelToDrone(idDrone);
-//                                }
-                                
-//                                catch(BO.NotImplementedException)
-//                                {
-//                                    bl.SendDroneToStation(idDrone);///שליחת רחפן לטעינה
-//                                }
-//                                catch
-//                                {
-//                                    throw new SimulatorException("No suitable package was found or the glider does not have a sufficient battery");
-//                                }
-//                            }
-//                            bl.CollectionAParcelByDroen(idDrone);//אוסף חבילה
-//                            break;
-//                        case DroneStatuses.Maintenance:
-//                            a();
-//                            break;
-//                        case DroneStatuses.Shipping:
-//                            bl.DeliveryOfAParcelByDrone(idDrone);//מבצע משלוח
-//                            break;
-//                        default:
-//                            break;
-//                    }}
-//                    Thread.Sleep(DELAY);
-                    
-                   
-                
-
-//            }
+    class Simulator
+    {
+        //static System.Windows.Timer myTimer = new System.Windows.Timer();
+        const int DELAY = 5000;// השהיה לחצי שניה
+        public double speed;//מהירות
+        private int idDrone;
+        private Action a;
+        private Func<bool> stop;
 
 
-//        }
+
+        public Simulator(IBL bl, int idDrone, Action a, Func<bool> stop)
+        {
+            //double battery;
+            while (!stop())
+            {
+                lock (bl)
+                {
+                    Drone drone = bl.GetDrone(idDrone);
+                    switch (drone.Status)
+                    {
+                        case DroneStatuses.Vacant:
+                            {
+                                try
+                                {
+                                    bl.UpdateParcelToDrone(idDrone);
+                                }
+
+                                catch (BO.NotImplementedException)
+                                {
+                                    bl.SendDroneToStation(idDrone);///שליחת רחפן לטעינה
+                                }
+                                catch
+                                {
+                                    throw new SimulatorException("No suitable package was found or the glider does not have a sufficient battery");
+                                }
+                            }
+                            bl.CollectionAParcelByDroen(idDrone);//אוסף חבילה
+                            break;
+                        case DroneStatuses.Maintenance:
+                            a();
+                            break;
+                        case DroneStatuses.Shipping:
+                            bl.DeliveryOfAParcelByDrone(idDrone);//מבצע משלוח
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                Thread.Sleep(DELAY);
 
 
-//    }
-//}
+
+
+            }
+
+
+        }
+
+
+    }
+}
 
